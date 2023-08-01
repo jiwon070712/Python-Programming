8f4717e7f3a6f40b6a53ae2b78f29535e26e83db
jiwon070712 <jiwon070712@gmail.com>
2023-08-01 13:16:14 +0900

    파이썬 기초 공부 2

--- Basic Study/triangle.py
+++ Basic Study/triangle.py
@@ -4,16 +4,11 @@ i = int(input())
 
 
 
-#for e in range (i-1):
-#    print('')
-#    for j in range(i-1-e):
-#        print(" ",end = '')
 
 
-print(end = '')
 for e in range (i):
     for j in range(i - e - 1):
-        print('-', end = '')
+        print(' ', end = '')
         
     for j in range(e+1):
         print('*', end = '')
--- Basic Study/while - 3.py
+++ Basic Study/while - 3.py
@@ -1,8 +1,7 @@
 a = 0
-b = 0
 
-#while b != 2:
-#    print('')
+
+
 while a!=3:
     b = 0
     print('')
