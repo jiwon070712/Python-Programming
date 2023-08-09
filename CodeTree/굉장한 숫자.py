bdf71aa1ef78c9b9aa71bf6f604d8e38322798bc
jiwon070712 <jiwon070712@gmail.com>
2023-08-09 22:52:34 +0900

    달에서 무게 구하기

--- CodeTree/굉장한 숫자.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-if n%2 == 1 and n%3 == 0:
-    answer = "true"
-elif n%2 == 0 and n%5 == 0:
-    answer = "true"
-else :
-    answer = "false"
-
-print(answer)
---
+++ CodeTree/소수점 맞춰 출력/달에서 무게 구하기.py
@@ -0,0 +1,9 @@
+ = 13
+b = 0.165
+c = a * b
+
+#print("%d * %.6f = %.6f" %(a,b,c))
+
+#print("{0} * {1:.6f} = {2:.6f}".format(a,b,c))
+
+print(f"{a} * {b:.6f} = {c:.6f}")
