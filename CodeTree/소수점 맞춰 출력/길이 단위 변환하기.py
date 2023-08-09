643c9707b1f7e3db4bd81590b226591ca1c4d02d
jiwon070712 <jiwon070712@gmail.com>
2023-08-09 23:07:10 +0900

    길이 단위 변환하기

---
+++ CodeTree/소수점 맞춰 출력/길이 단위 변환하기.py
@@ -0,0 +1,11 @@
+f = 30.48
+m = 160934
+
+#print("9.2ft = %.1fcm" %(9.2 * f))
+#print("1.3mi = %.1fcm" %(1.3 * m))
+
+#print("9.2ft = {0:.1f}cm".format(9.2 * f))
+#print("1.3mi = {0:.1f}cm".format(1.3 * m))
+
+print(f"9.2ft = {9.2 * f:.1f}cm")
+print(f"1.3mi = {1.3 * m:.1f}cm")
