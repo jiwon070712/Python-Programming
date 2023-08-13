f4d3b58ee738cb47c7ffa407b44a84e59d082067
jiwon070712 <jiwon070712@gmail.com>
2023-08-13 23:16:28 +0900

    합과 평균의 차

---
+++ CodeTree/연산자/합과 평균/합과 평균의 차.py
@@ -0,0 +1,14 @@
+i = input().split()
+a = int(i[0])
+b = int(i[1])
+c = int(i[2])
+sum1 = a + b + c
+avg = sum1 // 3
+print(sum1)
+print(avg)
+print(sum1 - avg)
+
+#arr = {a,b,c}
+#print(sum(arr))
+#print(sum(arr)//len(arr))
+#print(sum(arr)-sum(arr)//len(arr))
